bb754157660f6428a2c7170e067b0119c6370f8d
Coskun Caner <coshkun@users.noreply.github.com>
2017-11-23 10:56:49 +0300

    update

--- KuzeyYeli/KuzeyYeli.Extentions/TypeExtensions.cs
+++ KuzeyYeli/KuzeyYeli.Extentions/TypeExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,26 @@ namespace KuzeyYeli.Extensions
     {
         public static T MapTo<T>(this object source)
         {
+            Type hedeftipi = typeof(T);
+            Type kaynaktipi = source.GetType();
+
+            T result = Activator.CreateInstance<T>();
+
+            PropertyInfo[] hedefProps = hedeftipi.GetProperties();
+            PropertyInfo[] kaynakProps = kaynaktipi.GetProperties();
+
+            foreach (PropertyInfo kaynakProp in kaynakProps)
+            {
+                PropertyInfo hedefProp = hedefProps.FirstOrDefault(x => x.Name.ToLower() == kaynakProp.Name.ToLower());
+
+                if (hedefProp != null)
+                {
+                    hedefProp.SetValue(result, kaynakProp.GetValue(source)); 
+                }
+            }
+
+            if (result != null)
+                return result;
             return default(T);
         }
     }
